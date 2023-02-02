369738abb46910c1749aa18f377e8c76e3775c50
LEONE <leonardo.leone1998@gmail.com>
2023-02-02 19:13:16 -0300

    lab1 code modified - using do/while and rewriting the phrases

--- Intro_CS50X/Week_1_X/population.c
+++ Intro_CS50X/Week_1_X/population.c
@@ -12,14 +12,12 @@ int main(void)
     {
         printf("Enter the initial population size: ");
         scanf("%i", &initial_size);  
-        printf("Initial population = %i",initial_size);
-        printf(" \n");
-        if (initial_size < 9)
-        {
-            printf("The initial population must be higher than 9\n");
-        }
+        
+
     }
     while (initial_size < 9);
+    printf("Initial population = %i",initial_size);
+    printf(" \n");
 
     // TODO: Prompt for end size
 
@@ -29,15 +27,11 @@ int main(void)
     {
         printf("Enter the final population size: ");
         scanf("%i", &final_size);  
-        printf("Final population = %i",final_size);
-        printf(" \n");
-        if (final_size < initial_size)
-        {
-            printf("The final population must be equal or higher than the initial population\n");
-        }
-        
     }
     while (final_size < initial_size);
+
+    printf("Final population = %i",final_size);
+    printf(" \n");
 
     // TODO: Calculate number of years until we reach threshold
     int gain = initial_size/3;
